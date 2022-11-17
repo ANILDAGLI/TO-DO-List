3eb1425f6a62e308f751c46fef896a7baeb59656
ANIL DAĞLI <65742258+ANILDAGLI@users.noreply.github.com>
2022-11-17 22:23:00 +0300

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -1,1 +1,9 @@
-let yazi_alani = document.getElement 
+let yazi_alani = document.getElementById('yazi-alani');
+let ekle_butonu = document.getElemntById('ekle-butonu');
+let liste = document.getElementById('liste');
+let element = document.getElementById('li');
+
+ekle_butonu.addEventListener('click', () => {
+    let deger =yazi_alani.value;
+    console.log(deger);
+});
