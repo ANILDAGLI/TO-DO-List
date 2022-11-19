37bdc4ed9877a9529ba5bb3cb5d472cbb35fc778
ANIL DAĞLI <65742258+ANILDAGLI@users.noreply.github.com>
2022-11-19 23:28:06 +0300

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -2,11 +2,19 @@
 let yazi_alani = document.getElementById('yazi-alani');
 let ekle_butonu = document.getElemntById('ekle-butonu');
 let liste = document.getElementById('liste');
-let element = document.getElementById('li');
+let element = document.getElementsByTagName('li');
 
 ekle_butonu.addEventListener('click', () => {
-    let deger =yazi_alani.value;
-    console.log(deger);
+    let deger = yazi_alani.value;
+    if(!deger.length) return alert('Yazı kutusunu boş bırakamazsanız!')
+    let li = document.createElement('li');
+    li.innerText =deger;
+    liste.insertBefore(li, liste.childNodes[0]);
+    yazi_alani.value = "";
 });
 
-..
+liste.addEventListener('click', (e) => {
+    if(e.target.tagName === 'LI') {
+        e.target.classList.toggle('tamamlandi');
+}
+});
